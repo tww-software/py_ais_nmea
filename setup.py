d1521bec0aaf82702ec2761206e17d47500d83ff
Thomas Whittam <tw.whittam@gmail.com>
2020-07-14 16:48:46 +0100

    NEW VERSION 2020.7

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 
 setup(name='pyaisnmea',
-      version='2020.6',
+      version='2020.7',
       description='a Python 3 AIS NMEA 0183 decoder',
       author='Thomas W Whittam',
       url='https://github.com/tww-software/py_ais_nmea',
       license='MIT',
       packages=['pyaisnmea', 'pyaisnmea.messages', 'pyaisnmea.gui'],
+      install_requires=['tkinter'],
       include_package_data=True,
       zip_safe=False
 )
